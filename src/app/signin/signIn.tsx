64c4dcf7cdee42984b0ec42944688ab9e03b54dc
Mahinul <himahinulabid@gmail.com>
2024-01-17 18:16:20 +0600

    create module form

---
+++ src/app/signin/signIn.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBasic from '@ui/navBasic/navBasic';
+import styles from "./signIn.module.css";
+
+
+const SignInPage : React.FC = () => {
+  return (
+    <>
+      <NavBasic />
+      
+    </>
+  )
+}
+
+export default SignInPage;
