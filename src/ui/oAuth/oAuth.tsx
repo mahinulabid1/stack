4e23d3d1e5fb071b6d9544eb4e432a73e74f22f5
Mahinul <himahinulabid@gmail.com>
2024-01-17 15:51:19 +0600

    seperate OAuth module

---
+++ src/ui/oAuth/oAuth.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import styles from './oAuth.module.css'
+
+const OAuth: React.FC = () => {
+  return (
+    <>
+      <div className={'dFlex dSpaceBetween'}>
+        <button className={styles.openAuthBtn + ' dFlex dAlignItemsCenter'}>
+          <img src="./googleIcon.svg" alt="" />
+          Sign Up with Google
+        </button>
+
+        <button className={styles.openAuthBtn + ' dFlex dAlignItemsCenter'}>
+          <img className={styles.appleIcon} src="./appleIcon.svg" alt="" />
+          Sign Up with Apple ID
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default OAuth;
