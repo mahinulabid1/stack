21d13a07018a2c255a074fb718645c04c1a7ec7c
Mahinul <himahinulabid@gmail.com>
2024-01-17 19:54:54 +0600

    create template for multiple page

---
+++ src/app/ui/Template/signInAndSignUp/signInAndSignUpTemplate.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './style.module.css';
+
+interface ComponentProps {
+  Head  : any,
+  Form : any
+}
+
+const SignIAndSignUpTemplate: React.FC<ComponentProps> = ({Head, Form}) => {
+  return (
+    <>
+      <section className={styles.section}>
+        <div className={styles.centerContainer}>
+          {Head}
+          {Form}
+
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default SignIAndSignUpTemplate;
