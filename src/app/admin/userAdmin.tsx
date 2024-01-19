e716455b9a67ac1f75c22c0a33f27b954636e486
Mahinul <himahinulabid@gmail.com>
2024-01-19 22:37:54 +0600

    add route protect, redirect & more to admin

--- src/app/admin/userAdmin.tsx
+++ src/app/admin/userAdmin.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import styles from './userAdmin.module.css'
 import { useNavigate } from 'react-router-dom';
 import Sidebar from './module/sidebar/sidebar'
@@ -7,34 +7,52 @@ import { useCookies } from "react-cookie";
 import HelmetComponent from '../helmet';
 
 
-const UserAdmin:React.FC = () => {
+const UserAdmin: React.FC = () => {
+
+
+  const navigate = useNavigate(); // get the navigate object
+  const [cookie, setCookie, removeCookie] = useCookies<string>(['token', 'username']);
 
   // route protection
-  const navigate = useNavigate(); // get the navigate object
-  const [tokenCookie] = useCookies<string>(['token']);
-
-  useEffect(()=>{
-    if(!tokenCookie.token) {
-      navigate('/signIn');
+  useEffect(() => {
+    if (!cookie.token) {
+      navigate('/error/unwantedSignIn');
     }
   }, [])
 
 
-  return (
-    <>
-      <HelmetComponent title="Stack - Admin"/>
-      <div className={`dFlex ${styles.bodyContainer}`}>
-        {/* sidebar */}
-        <Sidebar />
+  const signOutHandler = (): void => {
+    removeCookie('token');
+    removeCookie('username')
+    navigate('/signIn');
+  }
 
-        {/* admin data component container */}
-        {/* different section( dashboard, user, sales ) will be rendered based in redux global state */}
-        <UserSection />
+  if (cookie.token) {
+    return (
+      <>
+        <HelmetComponent title="Stack - Admin" />
+        <div className={`dFlex ${styles.bodyContainer}`}>
+          {/* sidebar */}
+          <Sidebar />
+
+          {/* admin data component container */}
+          {/* different section( dashboard, user, sales ) will be rendered based in redux global state */}
+          <UserSection />
 
 
-      </div>
-    </>
-  )
+        </div>
+
+        <div className={styles.devMode}>
+          DEV MODE:
+          <button onClick={signOutHandler}>
+            Logout
+          </button>
+        </div>
+
+      </>
+    )
+  }
+
 }
 
 export default UserAdmin;
