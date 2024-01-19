a7bbf9d2240ed30119179ad93e470de7ddd8b7a3
Mahinul <himahinulabid@gmail.com>
2024-01-19 18:24:56 +0600

    modifiy login

--- src/app/loading/loading.tsx
+++ src/app/loading/loading.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from './loading.module.css'
 
-const Loading:React.FC = () => {
-  return(
+interface ComponentProps {
+  loadingMessage: string
+}
+
+const Loading: React.FC<ComponentProps> = ({loadingMessage}) => {
+  return (
     <>
-      <span className={styles.loader}></span>
+      <div className={`${styles.loaderContainer}`}>
+
+        <div className={styles.divCenter}>
+          <span className={styles.loader}></span>
+          <p className={styles.message}>{loadingMessage}</p>
+        </div>
+
+      </div>
+
     </>
   )
 }
