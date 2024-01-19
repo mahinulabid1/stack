e333213f368834abee87bf4b1357664261b5859f
Mahinul <himahinulabid@gmail.com>
2024-01-19 22:42:57 +0600

    add error page

--- src/routeHandler.tsx
+++ src/routeHandler.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import SignupPage from '@app/signup/signup.tsx'
 import SignInPage from "@app/signin/signIn.tsx";
 import UserAdmin from "@app/admin/userAdmin";
+import ErrorPage from "./app/errorPage/errorPage";
 
 
 const router = createBrowserRouter([
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: '/useradmin',
     element: <UserAdmin />
+  },
+  {
+    path: '/error/unwantedSignIn',
+    element: <ErrorPage heading="You are not logged in!☹️" redirectingTo="Sign In" route="/signin"/>
   }
 ]);
 
